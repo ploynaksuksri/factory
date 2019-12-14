b7bc00dd8b3f76444d312d5be079ed918966975e
Thongtra Naksuksri <thongtra.naksuksri@myworkpapers.com>
2019-12-14 13:00:00 +0700

    Added base repository

---
+++ WebApplication.Data/WebApplication.Data/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication.Data.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        //Task<IEnumerable<T>> GetAll();
+        //Task<T> Get(int id);
+        //Task Add(T obj);
+        //Task Update(T obj);
+        //Task Delete(int id);
+        //Task SaveChanges();
+
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int id);
+        TEntity Add(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(int id);
+        void SaveChanges();
+    }
+}
