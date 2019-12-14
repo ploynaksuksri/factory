cf6f20e7d4dd4e278acfa922d10ed9bc81294163
Thongtra Naksuksri <thongtra.naksuksri@myworkpapers.com>
2019-12-14 13:59:58 +0700

    Added new project

---
+++ WebApplication1/Controllers/ManufacturerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManufacturerController : ControllerBase
+    {
+        //todo: Create a method to get manufacturer name by model
+        //todo: create a method to return all manufacturers and number of models for these manufacturers
+    }
+}
